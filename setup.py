df00842c2fa38b168246d644f3a1877224479ac5
Arun Mahapatra <pratikarun@gmail.com>
2018-01-24 07:37:04 +0530

    Release v0.0.1

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy"
+        "Programming Language :: Python :: Implementation :: PyPy",
         "Intended Audience :: Developers",
         "Development Status :: 3 - Alpha",
     ],
