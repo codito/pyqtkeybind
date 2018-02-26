7b21e297b984a468d64193ff9695c388330cda11
Arun Mahapatra <pratikarun@gmail.com>
2018-02-26 07:23:20 +0530

    chore: increase timeout and ignore pytest_cache dir.

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -12,9 +12,12 @@ def test_hotkey_integration(xvfb):
     xdoexit = "xdotool key Control+Shift+E"
     cmd = ["python", "sample/hotkey.py"]
 
-    proc = Popen(cmd, stdout=PIPE)
-    time.sleep(3)
+    proc = Popen(cmd, stdout=PIPE, stderr=PIPE)
+    time.sleep(6)
     os.system(xdocmd)
     os.system(xdoexit)
 
-    assert "hello world" in proc.communicate()[0].decode("ascii")
+    stdout, stderr = proc.communicate()
+    print("stderr = {0}".format(stderr.decode("ascii")))
+    assert proc.returncode == 0
+    assert "hello world" in stdout.decode("ascii")
