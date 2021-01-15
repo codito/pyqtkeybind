e5f3af8b54c0aaad966f66d53c476593b4673fee
Arun Mahapatra <pratikarun@gmail.com>
2021-01-15 08:39:29 +0530

    Release v0.0.7

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ DESCRIPTION = "Global hotkey bindings for Windows and Linux for PyQt apps"
 URL = "https://github.com/codito/pyqtkeybind"
 EMAIL = "arun@codito.in"
 AUTHOR = "Arun Mahapatra"
-VERSION = (0, 0, 6)
+VERSION = (0, 0, 7)
 
 # Dependencies required for execution
 REQUIRED = [
